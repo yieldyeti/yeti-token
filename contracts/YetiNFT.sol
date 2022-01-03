b944d1ef1aed79d08cabc1f2e6785e882490bd72
Jimish Parekh <jim@pedalsup.com>
2022-01-03 16:47:58 +0530

    adding all the the bug fix

--- contracts/YetiNFT.sol
+++ contracts/YetiNFT.sol
@@ -1,54 +1,151 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract YetiNFT is ERC721Enumerable, Ownable {
-  using SafeMath for uint256;
-  using Counters for Counters.Counter;
+import "./@rarible/royalties/contracts/impl/RoyaltiesV2Impl.sol";
+import "./@rarible/royalties/contracts/LibPart.sol";
+import "./@rarible/royalties/contracts/LibRoyaltiesV2.sol";
 
-  Counters.Counter private _tokenIds;
+contract YetiNFT is ERC721Enumerable, Ownable, RoyaltiesV2Impl {
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
 
-  // string public baseTokenURI;
+    Counters.Counter private _tokenIds;
 
-  // constructor(string memory baseURI) ERC721("YetiNFT", "YNFT") {
-  //   setBaseURI(baseURI);
-  // }
-  constructor() ERC721("YetiNFT", "YNFT") {}
+    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
 
-  // function _baseURI() internal view virtual override returns (string memory) {
-  //   return baseTokenURI;
-  // }
-
-  // function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-  //   baseTokenURI = _baseTokenURI;
-  // }
-
-  function mint() public {
-    uint newTokenID = _tokenIds.current();
-    _safeMint(msg.sender, newTokenID);
-    _tokenIds.increment();
-  }
-
-  function tokensOfOwner(address _owner) external view returns (uint[] memory) {
-    uint tokenCount = balanceOf(_owner);
-    uint[] memory tokensId = new uint256[](tokenCount);
-
-    for (uint i = 0; i < tokenCount; i++) {
-        tokensId[i] = tokenOfOwnerByIndex(_owner, i);
+    struct details {
+        string tokenSymbol;
+        string tokenURL;
+        string tokenAttributes;
+        uint256 tokenPrice;
     }
 
-    return tokensId;
-  }
+    mapping(uint256 => details) private _properties;
 
-  function withdraw() public payable onlyOwner {
-    uint balance = address(this).balance;
-    require(balance > 0, "No ether left to withdraw");
+    constructor() ERC721("YetiNFT", "YNFT") {}
 
-    (bool success, ) = (msg.sender).call{value: balance}("");
-    require(success, "Transfer failed.");
-  }
+    function getTokenProperties(uint256 tokenId)
+        public
+        view
+        returns (details memory)
+    {
+        require(
+            super._exists(tokenId),
+            "ERC721URIStorage: URI query for nonexistent token"
+        );
+
+        return _properties[tokenId];
+    }
+
+    function _setProperties(
+        uint256 tokenId,
+        string memory tokenURI,
+        string memory tokenSYM,
+        string memory tokenAttributes,
+        uint256 tokenPrice
+    ) private {
+        require(
+            super._exists(tokenId),
+            "ERC721URIStorage: URI set of nonexistent token"
+        );
+        details memory detail = details(
+            tokenSYM,
+            tokenURI,
+            tokenAttributes,
+            tokenPrice
+        );
+        _properties[tokenId] = detail;
+    }
+
+    function mint(
+        string memory tokenURI,
+        string memory tokenSYM,
+        string memory tokenAttributes,
+        uint256 tokenPrice,
+        uint96 _percentagePoints
+    ) public onlyOwner {
+        uint256 newTokenID = _tokenIds.current();
+        _safeMint(msg.sender, newTokenID);
+        _setProperties(
+            newTokenID,
+            tokenURI,
+            tokenSYM,
+            tokenAttributes,
+            tokenPrice
+        );
+        if (_percentagePoints != 0) {
+            address payable royaltyowner = payable(address(msg.sender));
+            _percentagePoints = _percentagePoints * 100;
+            setRoyalties(newTokenID, royaltyowner, _percentagePoints);
+        }
+        _tokenIds.increment();
+    }
+
+    function setRoyalties(
+        uint256 _tokenId,
+        address payable _royaltiesReceipientAddress,
+        uint96 _percentageBasisPoints
+    ) private {
+        LibPart.Part[] memory _royalties = new LibPart.Part[](1);
+        _royalties[0].value = _percentageBasisPoints;
+        _royalties[0].account = _royaltiesReceipientAddress;
+        _saveRoyalties(_tokenId, _royalties);
+    }
+
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
+        external
+        view
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        LibPart.Part[] memory _royalties = royalties[_tokenId];
+        if (_royalties.length > 0) {
+            return (
+                _royalties[0].account,
+                (_salePrice * _royalties[0].value) / 10000
+            );
+        }
+        return (address(0), 0);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        if (interfaceId == LibRoyaltiesV2._INTERFACE_ID_ROYALTIES) {
+            return true;
+        }
+        if (interfaceId == _INTERFACE_ID_ERC2981) {
+            return true;
+        }
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokensOfOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        uint256 tokenCount = balanceOf(_owner);
+        uint256[] memory tokensId = new uint256[](tokenCount);
+
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+
+        return tokensId;
+    }
+
+    function withdraw() public payable onlyOwner {
+        Address.sendValue(payable(address(msg.sender)), address(this).balance);
+    }
 }
--- contracts/YetiToken.sol
+++ contracts/YetiToken.sol
@@ -1,46 +1,17 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract YetiToken is ERC20 {
-  using SafeMath for uint256;
-  
-  string constant NAME = "YETI";
-  string constant SYMBOL = "YETI";
-  uint256 TOTAL_SUPPLY = 100000000;
+    using SafeMath for uint256;
 
-  mapping(address => uint256) balances;
-  mapping(address => mapping (address => uint256)) allowed;
+    string constant NAME = "YETI";
+    string constant SYMBOL = "YETI";
+    uint256 constant TOTAL_SUPPLY = 100000000;
 
-  constructor() ERC20(NAME, SYMBOL) {
-    balances[msg.sender] = TOTAL_SUPPLY;
-  }
-
-  function totalSupply() override public view returns (uint256) {
-    return TOTAL_SUPPLY;
-  }
-
-  function balanceOf(address owner) override public view returns (uint) {
-    return balances[owner];
-  }
-
-  function transfer(address receiver, uint tokensAmount) override public returns (bool) {
-    require(tokensAmount <= balances[msg.sender]);
-    balances[msg.sender] = balances[msg.sender].sub(tokensAmount);
-    balances[receiver] = balances[receiver].add(tokensAmount);
-    emit Transfer(msg.sender, receiver, tokensAmount);
-    return true;
-  }
-
-  function approve(address delegate, uint amount) override public returns (bool) {
-    allowed[msg.sender][delegate] = amount;
-    emit Approval(msg.sender, delegate, amount);
-    return true;
-  }
-
-  function allowance(address owner, address delegate) override public view returns (uint) {
-    return allowed[owner][delegate];
-  }
+    constructor() ERC20(NAME, SYMBOL) {
+        _mint(msg.sender, TOTAL_SUPPLY * 10**uint256(decimals()));
+    }
 }
